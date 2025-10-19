dbdb781842fcc6cd04340cccb01b07af6bdc812e
Vlad Romanov <vladromeur@gmail.com>
2025-10-19 18:31:25 +0500

    get-stand

--- 06-templates/02-get-stand/src/app/app.component.ts
+++ 06-templates/02-get-stand/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core'
+import { CatComponent } from './animals/cat/cat.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'contentchildren-by-service';
+
+  @ViewChild('catComponent', {read: ViewContainerRef}) catComponent: CatComponent | undefined;
 }
