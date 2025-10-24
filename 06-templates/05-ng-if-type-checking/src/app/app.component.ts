3223e40bbfaf89307afeb8c58c551552d60e8bcd
Vlad Romanov <vladromeur@gmail.com>
2025-10-24 15:24:30 +0500

    templates-05-ng-if-type-checking

--- 06-templates/05-ng-if-type-checking/src/app/app.component.ts
+++ 06-templates/05-ng-if-type-checking/src/app/app.component.ts
@@ -3,16 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-if-type-checking';
-  data: {name: string; working: boolean} | null = null;
+  data: { name: string; working: boolean } | null = null;
 
   onLoadData() {
     this.data = {
       name: 'Egor',
       working: true,
-    }
+    };
   }
 }
