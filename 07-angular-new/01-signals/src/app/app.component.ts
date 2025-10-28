f917ba88c7fd4415fd1f237dba0fd47fc7f97bbd
Vlad Romanov <vladromeur@gmail.com>
2025-10-28 20:14:13 +0500

    angular-new-01-signals

--- 07-angular-new/01-signals/src/app/app.component.ts
+++ 07-angular-new/01-signals/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'signals';
+
+  readonly count = signal(0);
+
+  doubleCount = computed(() => this.count() * 2);
+
+  constructor() {
+    this.count.set(3);
+
+    setTimeout(() => {
+      this.count.set(5);
+    }, 3000);
+
+    setInterval(() => console.log(this.count()), 1000);
+  }
+
+  get name(): string {
+    console.log('Vlad');
+    return 'Vlad';
+  }
 }
