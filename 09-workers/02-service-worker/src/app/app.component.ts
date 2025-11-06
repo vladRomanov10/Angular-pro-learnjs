7464681551c0d9e2c75e3d9d17724a583dd1a9f7
Vlad Romanov <vladromeur@gmail.com>
2025-11-06 18:40:37 +0500

    workers-02-service-worker

--- 09-workers/02-service-worker/src/app/app.component.ts
+++ 09-workers/02-service-worker/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'service-worker';
+
+  constructor(swUpdate: SwUpdate) {}
 }
--- 09-workers/02-service-worker/src/app/app.module.ts
+++ 09-workers/02-service-worker/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:3000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
